6819575b4ea8f2d02d04adb299b60c5ff2e29497
sivareddy <sivareddy@gmail.com>
2018-04-11 08:10:58 +0530

    Added Maven project for sonarqube plugin analysis

---
+++ Sonarqube  Plugin For Maven/src/main/java/com/sivajavatechie/sonar/MavenSonar/App.java
@@ -0,0 +1,13 @@
+package com.sivajavatechie.sonar.MavenSonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
